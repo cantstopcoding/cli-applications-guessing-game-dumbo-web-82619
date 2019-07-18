3ab5d5647b6bcdcce602e1a467abd83e1ce2167e
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-18 17:52:03 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,6 @@ def run_guessing_game
   inp = gets.chomp 
   ran_num = rand(6) + 1 
   
-  puts "Goodbye!" if inp == "exit"
-  puts "Sorry! The computer guessed #{ran_num}." if inp.to_i == ran_num
-  puts "You guessed the correct number!" if inp == ran_num.to_s
+  puts 
 end 
   
