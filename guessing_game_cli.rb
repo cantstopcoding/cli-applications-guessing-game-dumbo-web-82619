3209303d48484f368c171862206b6ee5f8073e0c
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-18 17:56:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,8 +3,7 @@ def run_guessing_game
   inp = gets.chomp 
   ran_num = rand(6) + 1 
   
-  if inp != ran_num.to_s 
-    puts "Sorry! The computer guessed #{ran_num}."
-  end 
+  puts "Goodbye!" if inp == "exit"
+  
 end 
   
