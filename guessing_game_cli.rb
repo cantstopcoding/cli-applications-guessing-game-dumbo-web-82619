2166403695673711bc808f4612c90ac4e5765988
Jonathan Koss <jonathan_koss@yahoo.com>
2019-07-18 17:47:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   ran_num = rand(6) + 1 
   
   puts "Goodbye!" if inp == "exit"
-  puts 
+  puts "Sorry! The computerguessed #{ran_num}." if inp.to_i == ran_num
   puts "You guessed the correct number!" if inp.to_i == ran_num
 end 
   
